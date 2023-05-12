65fd5aad1cfd8ab9f3e7f7c36ca4b510868c65e2
localuser <localuser@VEA.LV>
2023-05-12 12:12:48 +0300

    Edit: Customer class with one to many link

---
+++ src/main/java/lv/venta/models/Customer.java
@@ -0,0 +1,30 @@
+package lv.venta.models;
+
+import java.util.Collection;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Table(name = "customer_table")//DB pusē izveidosies tabula
+@Entity
+public class Customer {
+	@Column(name = "Idc")//DB puse būs kolonna "id" un būs kā auto increment PK
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idc;
+	
+	@Column(name = "Name")//DB puse būs kolonna title
+	private String name;
+	
+	@Column(name = "Surname")//DB puse būs kolonna title
+	private String surname;
+	
+	@OneToMany(mappedBy = "customer")//sasaistam ar mainīgo no otras puses
+	private Collection<Product> allProducts;
+	
+}
